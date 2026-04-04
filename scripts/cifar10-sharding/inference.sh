25bd26e0f5066e52e1d368a64f94567df081bc8b
Vũ Hoàng Nhật Trường <hoangtruongnhatvu@gmail.com>
2026-04-04 08:27:23 +0000

    Truong Update Inference

--- scripts/cifar10-sharding/inference.sh
+++ scripts/cifar10-sharding/inference.sh
@@ -4,31 +4,33 @@ set -eou pipefail
 IFS=$'\n\t'
 
 if [[ ! -f cifar10-general-report.csv ]]; then
-    echo "nb_shards, forgot_shard_id, accuracy, precision_macro, recall_macro, f1_macro, training_time" > cifar10-general-report.csv
+    echo "nb_shards, nb_slices, forgot_shard_id, accuracy, precision_macro, recall_macro, f1_macro, training_time" > cifar10-general-report.csv
 fi
+
+slices=3
 
 ### Uncomment the case depending on the experiment you want to run.
 
 ### Inference without unlearning.
-metric=$(python inference.py --model cifar10 --shards 10 --slices 3 --dataset datasets/cifar10/datasetfile --batch_size 64 --dropout_rate 0.2 --container "cifar10" | tail -n 1)
+metric=$(python inference.py --model cifar10 --shards 10 --slices ${slices} --dataset datasets/cifar10/datasetfile --batch_size 64 --dropout_rate 0.2 --container "cifar10" | tail -n 1)
 cat containers/cifar10/times/shard-*.time > "containers/cifar10/times/times.tmp"
 time=$(python time_stats.py --container "cifar10" | awk -F ',' '{print $1}')
-echo "10,None,${metric},${time}" >> cifar10-general-report.csv
+echo "10,${slices},None,${metric},${time}" >> cifar10-general-report.csv
 
 ### Unlearning one shard at a time.
 
 # for i in {0..9}
 # do
-#   acc=$(python inference.py --model cifar10 --shards 10 --slices 1 --dataset datasets/cifar10/datasetfile --batch_size 64 --dropout_rate 0.2 --container "cifar10" --unlearn_shards ${i} | tail -n 1)
+#   acc=$(python inference.py --model cifar10 --shards 10 --slices ${slices} --dataset datasets/cifar10/datasetfile --batch_size 64 --dropout_rate 0.2 --container "cifar10" --unlearn_shards ${i} | tail -n 1)
 #   cat containers/cifar10/times/shard-*.time > "containers/cifar10/times/times.tmp"
 #   time=$(python time_stats.py --container "cifar10" | awk -F ',' '{print $1}')
-#   echo "10,${i},${acc},${time}" >> cifar10-general-report.csv 
+#   echo "10,${slices},${i},${acc},${time}" >> cifar10-general-report.csv 
 # done
 
 ### Unlearning n shards at a time.
 
 # forgot_shards=($(shuf -i 0-9 -n 4))
-# acc=$(python inference.py --model cifar10 --shards 10 --slices 1 --dataset datasets/cifar10/datasetfile --batch_size 64 --dropout_rate 0.2 --container "cifar10" --unlearn_shards $forgot_shards | tail -n 1)
+# acc=$(python inference.py --model cifar10 --shards 10 --slices ${slices} --dataset datasets/cifar10/datasetfile --batch_size 64 --dropout_rate 0.2 --container "cifar10" --unlearn_shards $forgot_shards | tail -n 1)
 # cat containers/cifar10/times/shard-*.time > containers/cifar10/times/times.tmp
 # time=$(python time_stats.py --container "cifar10" | awk -F ',' '{print $1}')
-# echo "10,\"[$(IFS=,; echo "${forgot_shards[*]}")]\",${acc},${time}" >> cifar10-general-report.csv
+# echo "10,${slices},\"[$(IFS=,; echo "${forgot_shards[*]}")]\",${acc},${time}" >> cifar10-general-report.csv
